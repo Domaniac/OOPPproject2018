bbdbe83cf3e3eabedbba95cae8fcc756e6f9eb25
Dominic Loh <dominicloh1881@gmail.com>
2018-12-12 02:29:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request
-from form import SignupForm
+from form import SignupForm, user_idf, user_id, user_rating, user_name, user_rtitle, user_rbody
 
 app = Flask(__name__)
 
@@ -16,18 +16,37 @@ def load_school():
 
 # @app.route('/ReviewForm1.html')
 # def review_form():
-    # return render_template("Dtemplate17112018/ReviewForm1.html")
+# return render_template("Dtemplate17112018/ReviewForm1.html")
 
 
 @app.route("/ReviewForm1.html", methods=["GET", "POST"])
 def signup():
     form = SignupForm()
+    user_idf()
     if request.method == "POST":
-        flash('Review submitted')
-        return render_template("Dtemplate17112018/index.html")
+        new_preferred_name = form.preferred_name.data
+        new_review_title = form.review_title.data
+        new_review_body = form.review_body.data
+        new_review_rating = form.review_rating.data
+        user_name.append(new_preferred_name)
+        user_rtitle.append(new_review_title)
+        user_rbody.append(new_review_body)
+        user_rating.append(new_review_rating)
+        flash("Review submitted! Your review id ( for editing it later) is: " + (
+                str(form.users_id[len(form.users_id) - 1]) + str(user_rating[1])))
+        return render_template("Dtemplate17112018/index.html", user_name=user_name, user_rtitle=user_rtitle,
+                               user_rbody=user_rbody, user_rating=user_rating, user_id=user_id)
     elif request.method == "GET":
         return render_template('Dtemplate17112018/TestReviewForm1.html', form=form)
 
+
+@app.route("/ReviewForm1")
+def asd():
+    form = SignupForm()
+    if request.method == "POST":
+        return render_template("Dtemplate17112018/index.html")
+    elif request.method == "GET":
+        return render_template('Dtemplate17112018/TestReviewForm1.html', form=form)
 
 
 if __name__ == '__main__':
--- form.py
+++ form.py
@@ -2,20 +2,39 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, BooleanField, RadioField
 from wtforms.validators import DataRequired, Email, Length
 
+user_id = [0]
+user_name = ["Test username"]
+user_rtitle = ["Test title"]
+user_rbody = ["Test review"]
+user_rating = [5]
+preferred_name = ""
+review_title = ""
+review_body = ""
+review_rating = 0
+
 
 class SignupForm(Form):
     preferred_name = StringField('Preferred name', validators=[DataRequired("Enter name")])
     review_title = StringField('Title of Review', validators=[DataRequired("Enter Review title")])
     review_body = TextAreaField("Review:", validators=[DataRequired("Enter Review body")])
-    review_rating = SelectField('Overall Rating:', choices=[('one stars', '1 Star'), ('two stars', '2 Star'),
-                                                            ('three stars', '3 Star'), ('four stars', '4 Star'),
-                                                            ('five stars', '5 Star')],
+    review_rating = SelectField('Overall Rating:', choices=[('1 Star', '1 Star'), ('2 Star', '2 Star'),
+                                                            ('3 Star', '3 Star'), ('4 Star', '4 Star'),
+                                                            ('5 Star', '5 Star')],
                                 validators=[DataRequired("Enter Review body")], default='five stars')
-    review_tags = RadioField('Post as anonymous?', choices=[('Y', 'Yes'), ('N', 'No')], default='N')
+    review_tags = RadioField('Post as anonymous?', choices=[('Yes', 'Yes'), ('No', 'No')], default='N')
     # ('Review tags:', choices=[('faculty', 'Faculty'), ('facilities', 'Facilities'),
     # ('food', 'Food'),
     # ('Co-curricular Activities', 'Co-curricular Activities'),
     # ('Courses and subjects offered',
     # 'Courses and subjects offered')],
     # validators=[DataRequired("Enter Review body")])
+    users_id = user_id # for app.py to flash current user id
     submit = SubmitField('Submit Review')
+
+
+def user_idf():
+    new_user_id = user_id[len(user_id)-1] + 1
+    user_id.append(new_user_id)
+    print('appended user_id')
+    for i in range(len(user_id)):
+        print(user_id[i])
